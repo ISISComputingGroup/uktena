b806b0d8b9e03c60e72da2d5f295897319ad3c24
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2026-06-11 23:33:26 +0100

    Warn if pythonw running

--- package_builder/dev_build_python.bat
+++ package_builder/dev_build_python.bat
@@ -14,7 +14,15 @@ taskkill /f /im caRepeater.exe
 set PYTHONRUNNING=
 for /f "usebackq" %%f in (`tasklist ^| findstr /i python.exe`) do set PYTHONRUNNING=1
 if not "%PYTHONRUNNING%" == "" (
-    @echo ERROR: Python processes running - please stop these / ibex server
+    @echo ERROR: Python.exe processes running - please stop these / ibex server
+    tasklist /fi  "IMAGENAME eq python.exe"
+    exit /b 1
+)
+set PYTHONWRUNNING=
+for /f "usebackq" %%f in (`tasklist ^| findstr /i pythonw.exe`) do set PYTHONWRUNNING=1
+if not "%PYTHONWRUNNING%" == "" (
+    @echo ERROR: Pythonw.exe processes running - please stop these / ibex server
+    tasklist /fi  "IMAGENAME eq pythonw.exe"
     exit /b 1
 )
 
